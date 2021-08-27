63938b4501cde2fff04b8b29aa148b286c424cd2
Syed <syedalimasroor.r@thoughtworks.com>
2021-08-27 14:04:49 +0530

    Adding the tests

--- src/test/java/com/batch16/cicd/demo/DemoApplicationTests.java
+++ src/test/java/com/batch16/cicd/demo/DemoApplicationTests.java
@@ -2,12 +2,23 @@ package com.batch16.cicd.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void test() {
+		assertEquals(2, 1 + 1);
 	}
 
+	@Test
+	void anotherTest() {
+		assertEquals(2, 1 + 1);
+	}
+
+	@Test
+	void yetAnotherTest() {
+		assertEquals(2, 1 + 1);
+	}
 }
